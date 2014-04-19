9dfb7941c4d4d68b402130cfdc8797f7912fb64e
James Reeves <jreeves@weavejester.com>
2014-04-19 22:07:51 +0100

    Add reloaded user functions

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.namespace "0.2.4"]
+                 [com.stuartsierra/component "0.2.1"]])
--- src/reloaded/repl.clj
+++ src/reloaded/repl.clj
@@ -1,6 +1,30 @@
-(ns reloaded.repl)
+(ns reloaded.repl
+  (:require [com.stuartsierra.component :as component]
+            [clojure.tools.namespace.repl :refer [disable-reload! refresh]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(disable-reload!)
+
+(def system nil)
+
+(defn init []
+  (binding [*out* *err*] (println "Warning: No system initializer function defined."))
+  :ok)
+
+(defn set-init! [init]
+  (alter-var-root #'init (fn [_] (fn [] (alter-var-root #'system (fn [_] (init))) :ok))))
+
+(defn start []
+  (alter-var-root #'system component/start)
+  :started)
+
+(defn stop []
+  (alter-var-root #'system #(when % (component/stop %)))
+  :stopped)
+
+(defn go []
+  (init)
+  (start))
+
+(defn reset []
+  (stop)
+  (refresh :after 'reloaded.repl/go))
