4cbe799e25378b5f32964f5eccaeef165dabc6fd
James Reeves <jreeves@weavejester.com>
2014-04-20 19:34:24 +0100

    Refactor initializer into separate var

--- src/reloaded/repl.clj
+++ src/reloaded/repl.clj
@@ -6,24 +6,25 @@
 
 (def system nil)
 
-(defn init []
-  (throw (Error. "No system initializer function found.")))
-
-(defn- make-init [init]
-  (fn []
-    (stop)
-    (alter-var-root #'system (fn [_] (init)))
-    :ok))
+(def ^:private initializer nil)
 
 (defn set-init! [init]
-  (alter-var-root #'init (fn [_] (make-init init))))
+  (alter-var-root #'initializer (constantly init)))
+
+(defn- stop-system [s]
+  (when s (component/stop s)))
+
+(defn init []
+  (if-let [init initializer]
+    (do (alter-var-root #'system #(do (stop-system %) (init))) :ok)
+    (throw (Error. "No system initializer function found."))))
 
 (defn start []
   (alter-var-root #'system component/start)
   :started)
 
 (defn stop []
-  (alter-var-root #'system #(when % (component/stop %)))
+  (alter-var-root #'system stop-system)
   :stopped)
 
 (defn go []
