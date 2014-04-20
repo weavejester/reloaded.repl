64c84a9846334c7d899dca2a3a1cef01f96016f1
James Reeves <jreeves@weavejester.com>
2014-04-20 15:38:02 +0100

    Add description and URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reloaded.repl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "REPL functions to support the reloaded workflow"
+  :url "REPL functions to support the reloaded workflow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
