3b5a196b25eacd2d3cd761eba6a736467cc293b2
James Reeves <jreeves@weavejester.com>
2014-04-21 18:51:27 +0100

    Fix project :url value

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reloaded.repl "0.1.0"
   :description "REPL functions to support the reloaded workflow"
-  :url "REPL functions to support the reloaded workflow"
+  :url "https://github.com/weavejester/reloaded.repl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
