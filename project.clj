53815ccf64742ad6d83103c6d29346e7cc705210
James Reeves <jreeves@weavejester.com>
2015-09-03 08:10:41 +0100

    Add suspendable resets
    Components that satisfy the Suspendable protocol can now preserve
    persistent connections across resets.
    
    Components that don't satisfy Suspendable will be stopped and started
    as normal.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [com.stuartsierra/component "0.2.3"]])
+                 [com.stuartsierra/component "0.2.3"]
+                 [suspendable "0.1.0"]])
--- src/reloaded/repl.clj
+++ src/reloaded/repl.clj
@@ -1,6 +1,7 @@
 (ns reloaded.repl
   (:require [com.stuartsierra.component :as component]
-            [clojure.tools.namespace.repl :refer [disable-reload! refresh]]))
+            [clojure.tools.namespace.repl :refer [disable-reload! refresh]]
+            [suspendable.core :as suspendable]))
 
 (disable-reload!)
 
@@ -12,12 +13,15 @@
   (alter-var-root #'initializer (constantly init)))
 
 (defn- stop-system [s]
-  (when s (component/stop s)))
+  (if s (component/stop s)))
+
+(defn- init-error []
+  (Error. "No system initializer function found."))
 
 (defn init []
   (if-let [init initializer]
     (do (alter-var-root #'system #(do (stop-system %) (init))) :ok)
-    (throw (Error. "No system initializer function found."))))
+    (throw (init-error))))
 
 (defn start []
   (alter-var-root #'system component/start)
@@ -35,6 +39,15 @@
   (alter-var-root #'system #(do (stop-system %) nil))
   :ok)
 
+(defn suspend []
+  (alter-var-root #'system #(if % (suspendable/suspend %)))
+  :suspended)
+
+(defn resume []
+  (if-let [init initializer]
+    (do (alter-var-root #'system #(suspendable/resume (init) %)) :resumed)
+    (throw (init-error))))
+
 (defn reset []
-  (clear)
-  (refresh :after 'reloaded.repl/go))
+  (suspend)
+  (refresh :after 'reloaded.repl/resume))
