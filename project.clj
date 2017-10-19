dc8acbe1c02d54bbfc88f0ea21e4d77ca62e4a9f
James Reeves <jreeves@weavejester.com>
2017-10-19 16:48:48 +0100

    Release 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reloaded.repl "0.2.3"
+(defproject reloaded.repl "0.2.4"
   :description "REPL functions to support the reloaded workflow"
   :url "https://github.com/weavejester/reloaded.repl"
   :license {:name "Eclipse Public License"
